a1a7d5a265d91ebf0788f08b52a0f4c0d7808cbf
Павел Грабовский <araa9977@mail.ru>
2024-09-29 01:00:01 +0700

    add class ModelDuck

---
+++ HeadFirst/Strategy/Duck/ModelDuck.cs
@@ -0,0 +1,15 @@
+﻿namespace HeadFirst.Strategy.Duck;
+
+public class ModelDuck : Duck
+{
+    public ModelDuck()
+    {
+        FlyBehavior = new FlyNoWay();
+        QuackBehavior = new Quack();
+    }
+
+    public override void Display()
+    {
+        Console.WriteLine("I 'm a model dick");
+    }
+}
