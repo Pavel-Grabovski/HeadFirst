32096944ecd6343660a993485f888961b9e5778a
Павел Грабовский <araa9977@mail.ru>
2024-10-11 01:15:55 +0700

    реализовал охрану царя

---
+++ HeadFirst/Observer/CSharp/Kingdom/Observers/Guard.cs
@@ -0,0 +1,26 @@
+﻿namespace HeadFirst.Observer.CSharp.Kingdom.Observers;
+
+public class Guard : IObserver<Order>
+{
+    private IDisposable unsubscriber;
+
+    public void OnCompleted()
+    {
+        Console.WriteLine("Царь умер или отрекся от престола");
+    }
+
+    public void OnError(Exception error)
+    {
+        Console.WriteLine(error.ToString());
+    }
+
+    public void OnNext(Order order)
+    {
+        Console.WriteLine(order.Message);
+    }
+
+    public virtual void Unsubscribe()
+    {
+        unsubscriber.Dispose();
+    }
+}
