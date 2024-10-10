91db44fae90280a2b3ff8ae64b75e4757401e42c
Павел Грабовский <araa9977@mail.ru>
2024-10-10 21:50:38 +0700

    реализация CurrentConditionsDisplay как на java

---
+++ HeadFirst/ObserverJavaVersion/CurrentConditionsDisplay.cs
@@ -0,0 +1,38 @@
+﻿namespace HeadFirst.ObserverJavaVersion;
+
+public class CurrentConditionsDisplay : IObserver, Observer.IDisplayElement
+{
+    Observable _observable;
+
+    private float _temperature;
+    private float _humidity;
+
+    public CurrentConditionsDisplay(Observable observable)
+    {
+        _observable = observable;
+        _observable.Register(this);
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Current conditions: {_temperature}°, F degrees and {_humidity} % humidity");
+    }
+
+    public void Update(float temp, float humidity, float pressure)
+    {
+        _temperature = temp;
+        _humidity = humidity;
+
+        Display();
+    }
+
+    public void Update(Observable observable, object? arg)
+    {
+        if(observable is WeatherData weatherData)
+        {
+            _temperature = weatherData.GetTemperature();
+            _humidity = weatherData.GetHumidity();
+            Display();
+        }
+    }
+}
