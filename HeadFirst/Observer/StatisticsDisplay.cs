5111703a2451ab9a67975e7ef0f615ad478b555e
PavelGrabovskiy <Araa9977@mail.ru>
2024-10-10 16:01:23 +0700

    add CurrentConditionsDisplay, StatisticsDisplay, ForecastDisplay

--- HeadFirst/Observer/StatisticsDisplay.cs
+++ HeadFirst/Observer/StatisticsDisplay.cs
@@ -1,12 +1,12 @@
 ﻿namespace HeadFirst.Observer;
 
-public class CurrentConditionsDisplay : IObserver, IDisplayElement
+public class StatisticsDisplay : IObserver, IDisplayElement
 {
     private float _temperature;
     private float _humidity;
     private ISubject _weatherData;
 
-    public CurrentConditionsDisplay(ISubject weatherData)
+    public StatisticsDisplay(ISubject weatherData)
     {
         _weatherData = weatherData;
         _weatherData.RegisterObserver(this);
@@ -14,7 +14,7 @@ public class CurrentConditionsDisplay : IObserver, IDisplayElement
 
     public void Display()
     {
-        Console.WriteLine($"Current conditions: {_temperature}°, F degrees and {_humidity} % humidity");
+        Console.WriteLine($"Statistics display: {_temperature}°, F degrees and {_humidity} % humidity");
     }
 
     public void Update(float temp, float humidity, float pressure)
---
+++ HeadFirst/Observer/WeatherStation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirst.Observer;
+
+public static class WeatherStation
+{
+    public static void Start()
+    {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentDisplay = new(weatherData);
+
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+        weatherData.SetMeasurements(80, 65, 30.4f);
+        weatherData.SetMeasurements(82, 70, 29.2f);
+        weatherData.SetMeasurements(78, 90, 29.2f);
+    }
+}
