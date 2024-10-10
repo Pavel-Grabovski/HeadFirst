f9ef877e4fbcaed0b2416f46a43e2d681b38957f
PavelGrabovskiy <Araa9977@mail.ru>
2024-10-10 15:04:17 +0700

    Создан класс WeatherData

--- HeadFirst/Observer/ISubject.cs
+++ HeadFirst/Observer/ISubject.cs
@@ -4,5 +4,5 @@ public interface ISubject
 {
     public void RegisterObserver(IObserver observer);
     public void RemoveObserver(IObserver observer);
-    public void NotifyObserver();
+    public void NotifyObservers();
 }
---
+++ HeadFirst/Observer/WeatherData.cs
@@ -0,0 +1,46 @@
+﻿namespace HeadFirst.Observer;
+
+public class WeatherData : ISubject
+{
+    private readonly List<IObserver> _observers;
+
+    private float _temperature;
+    private float _humidity;
+    private float _pressure;
+
+    public WeatherData()
+    {
+        _observers = new List<IObserver>();
+    }
+
+    public void NotifyObservers()
+    {
+        foreach(IObserver observer in _observers)
+        {
+            observer.Update(_temperature, _humidity, _pressure);
+        }
+    }
+
+    public void RegisterObserver(IObserver observer)
+    {
+        _observers.Add(observer);
+    }
+
+    public void RemoveObserver(IObserver observer)
+    {
+        _observers.Remove(observer);
+    }
+
+    public void MeasurementsChanged()
+    {
+        NotifyObservers();
+    }
+
+    public void SetMeasurements(float temperature, float humidity, float pressure)
+    {
+        _temperature = temperature;
+        _humidity = humidity;
+        _pressure = pressure;
+        MeasurementsChanged();
+    }
+}
