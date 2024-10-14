4cdd545d78fd9b1e42ace17297d99234dadb1f9e
PavelGrabovskiy <Araa9977@mail.ru>
2024-10-14 15:01:28 +0700

    Создан базовый класс Beverage и Espresso

---
+++ HeadFirst/Decorator/Beverage.cs
@@ -0,0 +1,9 @@
+﻿namespace HeadFirst.Decorator;
+public abstract class Beverage
+{
+    protected string Description = "Unknown Beverage";
+
+    public abstract decimal Cost();
+
+    public virtual string GetDescription() => Description;
+}
---
+++ HeadFirst/Decorator/Espresso.cs
@@ -0,0 +1,12 @@
+﻿namespace HeadFirst.Decorator;
+
+public class Espresso : Beverage
+{
+    private decimal _cost = 1.99m;
+    public Espresso() 
+    {
+        Description = "Espresso";
+    }
+
+    public override decimal Cost() => _cost;
+}
