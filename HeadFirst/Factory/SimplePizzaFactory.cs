bb778759ef9638f545e66d2268baaeced21038a7
PavelGrabovskiy <Araa9977@mail.ru>
2024-10-15 17:09:42 +0700

    add class SimplePizzaFactory

---
+++ HeadFirst/Factory/SimplePizzaFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace HeadFirst.Factory;
+
+public class SimplePizzaFactory
+{
+    public Pizza CreatePizza(PizzaTypeEnum type)
+    {
+        Pizza pizza;
+
+        if (type == PizzaTypeEnum.Cheese)
+        {
+            pizza = new CheesePizza();
+        }
+        else if (type == PizzaTypeEnum.Pepperoni)
+        {
+            pizza = new PepperoniPizza();
+        }
+        else if (type == PizzaTypeEnum.Clam)
+        {
+            pizza = new ClamPizza();
+        }
+        else if (type == PizzaTypeEnum.Veggie)
+        {
+            pizza = new VeggiePizza();
+        }
+        else
+        {
+            throw new ArgumentException("Unknown pizza type");
+        }
+
+        return pizza;
+    }
+}
