9714aeff578a4304639f2c61d4ef1638a3a662ab
PavelGrabovskiy <Araa9977@mail.ru>
2024-10-15 18:47:41 +0700

    add class PizzaStore

---
+++ HeadFirst/Factory/PizzaStore.cs
@@ -0,0 +1,24 @@
+﻿namespace HeadFirst.Factory;
+
+public class PizzaStore
+{
+    SimplePizzaFactory _factory;
+
+    public PizzaStore(SimplePizzaFactory factory)
+    {
+        _factory = factory;
+    }
+
+    public Pizza OrderPizza(PizzaTypeEnum type)
+    {
+        Pizza pizza;
+
+        pizza = _factory.CreatePizza(type);
+
+        pizza.Prepare();
+        pizza.Bake();
+        pizza.Cut();
+        pizza.Box();
+        return pizza;
+    }
+}
