0384262cf72a64b53714de0d6c0dc19054b6dd6a
Павел Грабовский <araa9977@mail.ru>
2024-10-17 00:28:17 +0700

    Новые стили пиццы

---
+++ HeadFirst/Factory/Pizzas/ChicagoStyleVeggiePizza.cs
@@ -0,0 +1,25 @@
+﻿namespace HeadFirst.Factory.Pizzas
+{
+    internal class ChicagoStyleVeggiePizza : Pizza
+    {
+        public override void Bake()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Box()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Cut()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Prepare()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HeadFirst/Factory/Pizzas/NYStyleVeggiePizza.cs
@@ -0,0 +1,24 @@
+﻿namespace HeadFirst.Factory.Pizzas;
+
+public class NYStyleVeggiePizza : Pizza
+{
+    public override void Bake()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Box()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Cut()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Prepare()
+    {
+        throw new NotImplementedException();
+    }
+}
