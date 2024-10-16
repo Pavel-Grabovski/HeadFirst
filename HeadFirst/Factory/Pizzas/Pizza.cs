71412e0ae51246a111999669a35d44b2963f41aa
Павел Грабовский <araa9977@mail.ru>
2024-10-17 01:39:57 +0700

    новые свойства для Pizza

--- HeadFirst/Factory/Pizzas/Pizza.cs
+++ HeadFirst/Factory/Pizzas/Pizza.cs
@@ -2,6 +2,14 @@
 
 public abstract class Pizza
 {
+    public string Name { get; set; }
+    
+    public string Dough { get; set; }
+
+    public string Sauce { get; set; }
+
+    public List<string> toppings { get; set; } = new List<string>();
+
     public abstract void Prepare();
 
     public abstract void Bake();
