7d520f3d93c04a69ab5940662249e55bfc4754c1
PavelGrabovskiy <Araa9977@mail.ru>
2024-10-23 21:06:24 +0700

    добавлены классы ингридиентов в поле класса Pizza

--- HeadFirst/Factory/Pizzas/Pizza.cs
+++ HeadFirst/Factory/Pizzas/Pizza.cs
@@ -1,12 +1,14 @@
-﻿namespace HeadFirst.Factory.Pizzas;
+﻿using HeadFirst.Factory.Ingredients;
+
+namespace HeadFirst.Factory.Pizzas;
 
 public abstract class Pizza
 {
     public string Name { get; set; }
     
-    public string Dough { get; set; }
+    public Dough Dough { get; set; }
 
-    public string Sauce { get; set; }
+    public Sauce Sauce { get; set; }
 
     public List<string> toppings { get; set; } = new List<string>();
 
