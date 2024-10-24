3fa23c995b903831a50a20046cbe120cfd0b1e3b
PavelGrabovskiy <Araa9977@mail.ru>
2024-10-24 19:13:00 +0700

    добавил фабрику в NYPizzaStore

--- HeadFirst/Factory/NYPizzaStore.cs
+++ HeadFirst/Factory/NYPizzaStore.cs
@@ -8,21 +8,27 @@ public class NYPizzaStore : PizzaStore
     {
         Pizza pizza;
 
+        IPizzaIngredientFactory _ingredientFactory = new NYPizzaIngredientFactory();
+
         if (type == PizzaTypeEnum.Cheese)
         {
-            pizza = new NYStyleCheesePizza();
+            pizza = new NYStyleCheesePizza(_ingredientFactory);
+            pizza.SetName("New York Style Cheese Pizza");
         }
         else if (type == PizzaTypeEnum.Pepperoni)
         {
-            pizza = new NYStylePepperoniPizza();
+            pizza = new NYStylePepperoniPizza(_ingredientFactory);
+            pizza.SetName("New York Style Pepperoni Pizza");
         }
         else if (type == PizzaTypeEnum.Clam)
         {
-            pizza = new NYStyleClamPizza();
+            pizza = new NYStyleClamPizza(_ingredientFactory);
+            pizza.SetName("New York Style Clam Pizza");
         }
         else if (type == PizzaTypeEnum.Veggie)
         {
-            pizza = new NYStyleVeggiePizza();
+            pizza = new NYStyleVeggiePizza(_ingredientFactory);
+            pizza.SetName("New York Style Veggie Pizza");
         }
         else
         {
