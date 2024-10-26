ec5d2d7a4a04b21ea63689292d183f256b130bc6
Павел Грабовский <araa9977@mail.ru>
2024-10-27 02:39:13 +0700

    add CaliforniaPizzaIngredientFactory

---
+++ HeadFirst/Factory/IngredientFactory/CaliforniaPizzaIngredientFactory.cs
@@ -0,0 +1,46 @@
+﻿using HeadFirst.Factory.Ingredients.Cheese;
+using HeadFirst.Factory.Ingredients.Clams;
+using HeadFirst.Factory.Ingredients.Dough;
+using HeadFirst.Factory.Ingredients.Pepperoni;
+using HeadFirst.Factory.Ingredients.Sauce;
+using HeadFirst.Factory.Ingredients.Veggies;
+namespace HeadFirst.Factory.IngredientFactory;
+
+public class CaliforniaPizzaIngredientFactory : IPizzaIngredientFactory
+{
+    public ICheese CreateCheese()
+    {
+        return new GoatCheese();
+    }
+
+    public IClam CreateClam()
+    {
+        return new Calamari();
+    }
+
+    public IDough CreateDough()
+    {
+        return new VeryThinCrust();
+    }
+
+    public IPepperoni CreatePepperoni()
+    {
+        return new SlicedPepperoni();
+    }
+
+    public ISauce CreateSauce()
+    {
+        return new BruschettaSauce();
+    }
+
+    public IVegetable[] CreateVeggies()
+    {
+        return new IVegetable[]
+        {
+            new Spinach(),
+            new BlackOlives(),
+            new EggPlant()
+        };
+    }
+}
+
