acd4d33c7b722f9b568c4c59cc11777e197390c8
Павел Грабовский <araa9977@mail.ru>
2024-10-29 00:14:21 +0700

    add Singleton

---
+++ HeadFirst/Singleton/Singleton.cs
@@ -0,0 +1,14 @@
+﻿namespace HeadFirst.Singleton;
+
+public class Singleton
+{
+    private static Singleton? uniqueInstance;
+
+    public static Singleton GetInstance()
+    {
+        if (uniqueInstance == null)
+            uniqueInstance = new Singleton();
+
+        return uniqueInstance;
+    }
+}
