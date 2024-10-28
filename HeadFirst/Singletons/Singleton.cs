46be57c070375a129631f56a87976a2ef1eda761
Павел Грабовский <araa9977@mail.ru>
2024-10-29 00:31:48 +0700

    Переменовал простр имен для одиночек

--- HeadFirst/Singletons/Singleton.cs
+++ HeadFirst/Singletons/Singleton.cs
@@ -1,13 +1,18 @@
-﻿namespace HeadFirst.Singleton;
+﻿namespace HeadFirst.Singletons;
 
 public class Singleton
 {
     private static Singleton? uniqueInstance;
 
+    public Guid Guid;
+
     public static Singleton GetInstance()
     {
         if (uniqueInstance == null)
+        {
             uniqueInstance = new Singleton();
+            uniqueInstance.Guid = Guid.NewGuid();
+        }
 
         return uniqueInstance;
     }
