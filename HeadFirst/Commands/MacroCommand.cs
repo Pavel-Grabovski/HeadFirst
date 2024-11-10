058e9ef2f9fd9e247928bb082c4ca8130ffb15e4
Pavel Grabovskiy <Araa9977@mail.ru>
2024-11-11 06:03:13 +0700

    MacroCommand

---
+++ HeadFirst/Commands/MacroCommand.cs
@@ -0,0 +1,27 @@
+﻿namespace HeadFirst.Commands;
+
+public class MacroCommand : ICommand
+{
+    private readonly ICommand[] _commands;
+
+    public MacroCommand(ICommand[] commands)
+    {
+        _commands = commands;
+    }
+
+    public void Execute()
+    {
+        for (int i = 0; i < _commands.Length; i++)
+        {
+            _commands[i].Execute();
+        }
+    }
+
+    public void Undo()
+    {
+        for (int i = 0; i < _commands.Length; i++)
+        {
+            _commands[i].Undo();
+        }
+    }
+}
