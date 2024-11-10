a51cc2ae17ae2584dd455e8abdead4a6f1a3433b
Pavel Grabovskiy <Araa9977@mail.ru>
2024-11-11 02:06:09 +0700

    Undo in RemoteControl

--- HeadFirst/Commands/Controls/RemoteControl.cs
+++ HeadFirst/Commands/Controls/RemoteControl.cs
@@ -5,20 +5,21 @@ namespace HeadFirst.Commands.Controls;
 public class RemoteControl
 {
     private ICommand[] onCommands;
-    private ICommand[] OffCommands;
-
+    private ICommand[] offCommands;
+    private ICommand undoCommand;
     public RemoteControl()
     {
         onCommands = new ICommand[7];
-        OffCommands = new ICommand[7];
+        offCommands = new ICommand[7];
 
         ICommand noCommand = new NoCommand();
 
         for (int i = 0; i < 7; i++)
         {
             onCommands[i] = noCommand;
-            OffCommands[i] = noCommand;
+            offCommands[i] = noCommand;
         }
+        undoCommand = noCommand;
     }
 
     private class NoCommand : ICommand
@@ -31,17 +32,24 @@ public class RemoteControl
     public void SetCommand(int slot, ICommand onCommand, ICommand offCommand)
     {
         onCommands[slot] = onCommand;
-        OffCommands[slot] = offCommand;
+        offCommands[slot] = offCommand;
     }
 
     public void OnButtonPushed(int slot)
     {
         onCommands[slot].Execute();
+        undoCommand = onCommands[slot];
     }
 
     public void OffButtonPushed(int slot)
     {
-        OffCommands[slot].Execute();
+        offCommands[slot].Execute();
+        undoCommand = offCommands[slot];
+    }
+
+    public void undoButtonPushed()
+    {
+        undoCommand.Undo();
     }
 
     public override string ToString()
@@ -50,9 +58,11 @@ public class RemoteControl
 
         sb.Append("\n ------- Remote Contrlol ------- \n");
 
+        sb.Append($"Undo: {undoCommand.ToString()}\n");
+
         for (int i = 0; i < onCommands.Length; i++)
         {
-            sb.Append($"Slot: {i} {onCommands[i].ToString()} -- {OffCommands[i].ToString()} \n");
+            sb.Append($"Slot: {i} {onCommands[i].ToString()} -- {offCommands[i].ToString()} \n");
         }
 
         return sb.ToString();
