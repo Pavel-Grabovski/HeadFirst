f5a260fa5335b2d907de3fc2721630ae8a55e8e8
Pavel Grabovskiy <Araa9977@mail.ru>
2024-11-30 03:03:23 +0700

    Tea  => TeaWithHook

---
+++ HeadFirst/TemplateMethod/TeaWithHook.cs
@@ -0,0 +1,33 @@
+﻿namespace HeadFirst.TemplateMethod;
+
+public class TeaWithHook : CaffeineBeverageWithHook
+{
+    public override void AddComponents()
+    {
+        Console.WriteLine("Adding lemon");
+
+    }
+
+    public override void Brew()
+    {
+        Console.WriteLine("Steeping the tea");
+    }
+
+    public override bool IsCustomerWantsCondiments()
+    {
+        string answer = GetUserInput();
+        if (answer.ToLower() == "y")
+            return true;
+
+        return false;
+    }
+
+    private string GetUserInput()
+    {
+        Console.Write("Would you like lemon with your tea? (y/n): ");
+
+        string input = Console.ReadLine();
+
+        return input;
+    }
+}
