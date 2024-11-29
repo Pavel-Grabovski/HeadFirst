841fd50111ddd1731028fd8ed59dec1abb3d6ce8
Pavel Grabovskiy <Araa9977@mail.ru>
2024-11-30 03:00:02 +0700

    Coffee => CoffeeWithHook

---
+++ HeadFirst/TemplateMethod/CoffeeWithHook.cs
@@ -0,0 +1,32 @@
+﻿namespace HeadFirst.TemplateMethod;
+
+public class CoffeeWithHook : CaffeineBeverageWithHook
+{
+    public override void AddComponents()
+    {
+        Console.WriteLine("Adding sugar and milk");
+    }
+
+    public override void Brew()
+    {
+        Console.WriteLine("Dripping Coffee through filter");
+    }
+
+    public override bool IsCustomerWantsCondiments()
+    {
+        string answer = GetUserInput();
+        if(answer.ToLower() == "y")
+            return true;
+
+        return false;  
+    }
+
+    private string GetUserInput()
+    {
+        Console.Write("Would you like milk and sugar with your coffee? (y/n): ");
+
+        string input = Console.ReadLine();
+
+        return input;
+    }
+}
