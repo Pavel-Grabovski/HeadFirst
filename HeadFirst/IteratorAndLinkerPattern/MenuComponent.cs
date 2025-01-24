ba4b07e3525bc7e2f387ca66a1fd1ba6414faa11
PavelGrabovskiy <Araa9977@mail.ru>
2025-01-24 19:59:33 +0700

    add abstract class MenuComponent

---
+++ HeadFirst/IteratorAndLinkerPattern/MenuComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace HeadFirst.IteratorAndLinkerPattern;
+
+public abstract class MenuComponent
+{
+    public abstract void Add(MenuComponent menuComponent);
+
+    public abstract void Remove(MenuComponent menuComponent);
+
+    public abstract MenuComponent GetChild(int index);
+
+    public abstract string GetName();
+
+    public abstract string GetDescription();
+
+    public abstract string GetPrice();
+
+    public abstract bool IsVegetarian();
+
+    public abstract void Print();
+}
