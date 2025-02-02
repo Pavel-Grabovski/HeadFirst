e409b086d37a9b63bdc4621f5a3f8472914b6048
Pavel Grabovskiy <Araa9977@mail.ru>
2025-02-03 02:02:52 +0700

    Компоновка уток

---
+++ HeadFirst/CompositionPatterns/Flock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirst.CompositionPatterns;
+
+public class Flock : IQuackable
+{
+    List<IQuackable> quackers = new List<IQuackable>();
+
+    public void Add(IQuackable quacker)
+    {
+        quackers.Add(quacker);
+    }
+
+    public void Quack()
+    {
+        IEnumerator<IQuackable> iterator = quackers.GetEnumerator();
+        while (iterator.MoveNext())
+        {
+            IQuackable quacker = iterator.Current;
+            quacker.Quack();
+        }
+
+        //foreach(var quacker in quackers)
+        //{
+        //    quacker.Quack();
+        //}
+    }
+}
