502dd3e601a6e52ed73691b9ed59dfb8ed54bb80
Pavel Grabovskiy <Araa9977@mail.ru>
2025-02-09 02:59:40 +0700

    Добавил интерфейс IBeatModel и классы пустышки BeatObserver и BPMObserver

---
+++ PlayerMP3/Models/BPMObserver.cs
@@ -0,0 +1,6 @@
+﻿namespace PlayerMP3.Models;
+
+public class BPMObserver
+{
+
+}
---
+++ PlayerMP3/Models/IBeatModel.cs
@@ -0,0 +1,20 @@
+﻿namespace PlayerMP3.Models;
+
+public interface IBeatModel
+{
+    public void On();
+    
+    public void Off();
+
+    public void SetBPM(int bpm);
+    
+    public int GetBPM();
+
+    public void RegisterObserver(BeatObserver observer);
+
+    public void RemoveObserver(BeatObserver observer);
+
+    public void RegisterObserver(BPMObserver observer);
+
+    public void RemoveObserver(BPMObserver observer);
+}
