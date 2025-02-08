1917a4cf6abf82b0d5c1df2da5d397ba9f48c4ec
Pavel Grabovskiy <Araa9977@mail.ru>
2025-02-09 02:41:28 +0700

    Сдалал показ View вместе с Control при запуске окна

--- PlayerMP3/Program.cs
+++ PlayerMP3/Program.cs
@@ -11,6 +11,11 @@ namespace PlayerMP3
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+            Windows.View view = new Windows.View();
+            view.Show();
+
+
             Application.Run(new Control());
         }
     }
