79bf1c5f953c985bbd20260dfec180c0a1f89fed
Pavel Grabovskiy <Araa9977@mail.ru>
2025-02-10 01:13:44 +0700

    ADD interface IController

---
+++ PlayerMP3/Controllers/IController.cs
@@ -0,0 +1,14 @@
+﻿namespace PlayerMP3.Controllers;
+
+public interface IController
+{
+    public void Start();
+    
+    public void Stop();
+
+    public void IncreaseBPM();
+    
+    public void DecreaseBPM();
+
+    public void SetBPM(int bpm);
+}
